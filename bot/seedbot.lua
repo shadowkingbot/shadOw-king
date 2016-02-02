cf951030a9279578a0e33bb80cd3443907668780
mobin <mobin.bbbb@gmail.com>
2016-02-02 17:17:10 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {175636120},--Sudo users
+    sudo_users = {159040152},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
